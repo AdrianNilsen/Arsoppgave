c43686e24c205c9139f7bc7c8be43c2c1273d388
AdrianNilsen <157589334+AdrianNilsen@users.noreply.github.com>
2025-05-21 10:05:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['add_workout'])) {
 
 if ($valgtdato < $nowdato) {
     echo "<p style='color: red;'>Dato kan ikke være i fortiden</p>";
+    exit();
 }
 
 
