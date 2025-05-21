bbce60ad8e60c273951749c1aa040f4fecc1113d
AdrianNilsen <157589334+AdrianNilsen@users.noreply.github.com>
2025-05-21 09:59:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,12 +20,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['add_workout'])) {
 if ($valgtdato < $nowdato) {
     echo "<p style='color: red;'>Dato kan ikke være i fortiden</p>";
     die();
-else {
+}
+
+
     $stmt = $conn->prepare("INSERT INTO workouts (user_id, workout_date, workout_text) VALUES (?, ?, ?)");
     $stmt->bind_param("iss", $user_id, $date, $text);
     $stmt->execute();
     $stmt->close();
-    }
 }
 
 // Hent workouts
